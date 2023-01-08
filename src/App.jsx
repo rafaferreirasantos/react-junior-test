832f7b4c4bc69192e3063828324e9c9a0bc68609
Rafael Ferreira dos Santos <osmarditto@gmail.com>
2023-01-08 18:52:44 -0300

    Solved

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,55 @@ import { useState } from 'react';
 // * Não é permitido usar refs
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
 // todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
 // todo - Desabilite o botão de Login equanto você está executando o login.
 // todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
 // todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [running, setRunning] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const handleInputChange = ({ setFunction, event }) => {
+    const { value } = event.target;
+    setFunction(value)
+  }
+
+  const handleSubmit = (event) => {
+    setErrorMessage('')
+    setRunning(true)
+    const result = login({
+      email,
+      password
+    }).then((r) => {
+      alert('Login sucessful.')
+    }).catch((r) => {
+      setErrorMessage(r.message)
+    }).finally(() => {
+      setRunning(false)
+    })
+  }
+
   return (
     <div className='wrapper'>
       <div className='login-form'>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        <div className='errorMessage' style={{ display: errorMessage ? 'block' : 'none' }}>{errorMessage}</div>
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input id={'email'} type={'email'} autoComplete='off' value={email} onChange={(event) => { handleInputChange({ email, setFunction: setEmail, event }) }} />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input id={'password'} type={'password'} value={password} onChange={(event) => { handleInputChange({ email, setFunction: setPassword, event }) }} />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          <button disabled={!email || password.length < 6 || running} onClick={handleSubmit}>Login</button>
         </div>
       </div>
     </div>
